53646be39d7bc0fc08b25dcb42a6b99ee5b3f403
Gábor Gazdag <49676027+rhytme@users.noreply.github.com>
2021-08-16 18:08:32 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include ("./includes/SystemlogClass.php");
 header('Cache-Control: no cache'); //no cache
 //session_cache_limiter('private_no_expire'); // works
 //session_cache_limiter('public'); // works too
-
+//test//
 
 if (isset($_COOKIE["PHPSESSID"])) {session_id($_COOKIE["PHPSESSID"]);}
 session_start();
