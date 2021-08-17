f063038aa34a7a2aed8aa19f0fe5bf3b18f0ba1d
rhytme <gazdaggaborg@gmail.com>
2021-08-17 15:47:00 +0200

    test

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ include ("./includes/SystemlogClass.php");
 header('Cache-Control: no cache'); //no cache
 //session_cache_limiter('private_no_expire'); // works
 //session_cache_limiter('public'); // works too
-//tesasdasda2t//
+//tesasdasdaaasdasfs2t//
 
 if (isset($_COOKIE["PHPSESSID"])) {session_id($_COOKIE["PHPSESSID"]);}
 session_start();
